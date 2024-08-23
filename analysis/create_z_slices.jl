2e50a1dbe581dd8edf06861b37359aac59e3224c
jewel <jewelc1005@gmail.com>
2024-08-23 13:00:13 -0400

    Update create_z_slices.jl

--- analysis/create_z_slices.jl
+++ analysis/create_z_slices.jl
@@ -1,7 +1,32 @@
+# Opening main file
+using Oceananigans
+using CairoMakie
+using JLD2
+using Oceananigans.Units
+using Printf
 
+filename = "../../[FOLDERNAME]/[FILENAME].jld2"
+file = jldopen(filename)
+
+# Retrieve simulation velocity data, masking immersed field for velocity fields
+u′_t = FieldTimeSeries(filename, "u′")
+v′_t = FieldTimeSeries(filename, "v′")
+w_t = FieldTimeSeries(filename, "w")
+times = u′_t.times
+
+# Constants and grid data
+Lx = file["grid/underlying_grid/Lx"]
+Ly = file["grid/underlying_grid/Ly"]
+Lz = file["grid/underlying_grid/Lz"]
+Nx = file["grid/underlying_grid/Nx"]
+Ny = file["grid/underlying_grid/Ny"]
+Nz = file["grid/underlying_grid/Nz"]
+
+ω₂ = 0.00014 # default value, change if value of ω₂ changes
+T₂ = 2π / ω₂
+coriolis_f = file["coriolis/f"]
 
 # Plotting u′, v′, w (z-slices)
-
 slice_point = trunc(Int, Nz/2) # Alter this for slices at different values of z
 
 umax = maximum(abs, u′_t[end][:, :, slice_point])
@@ -45,3 +70,12 @@ hm_w = heatmap!(ax_w, xw, yw, w; colorrange = (-wmax, wmax), colormap = :balance
 Colorbar(fig[4, 2], hm_w, label = "m s⁻¹")
 
 fig
+
+# Animation of u′, v′, w (z-slices)
+@info "Making an animation from saved data..."
+
+frames = 1:length(times)
+
+record(fig, filename * ".mp4", frames, framerate=16) do i
+    n[] = i
+end
