0b87ece7f89c84353cd80c4a9d59f9b00e669efb
jewel <jewelc1005@gmail.com>
2024-08-23 14:02:57 -0400

    Update nondim_parameters.jl

--- src/functions/nondim_parameters.jl
+++ src/functions/nondim_parameters.jl
@@ -1,4 +1,6 @@
-default_inputs = (; δ=0.3, E=0.5, β=0.5, B=14, H=3000, Nx=300, Ny=300, Nz=150, T₂=12.421)
+default_inputs = (; δ=0.3, E=0.5, β=0.5, B=14, H=3000, Nx=512, Ny=512, Nz=256, ω₂=0.00014)
+# (; height ratio, relative steepness, frequency ratio (f/ω₂), frequency ratio (N/ω₂), 
+     domain height, x grid size, y grid size, z grid size, tidal frequency)
 
 @inline function create_simulation_parameters(input_parameters=(; ))
     ip = (; default_inputs..., input_parameters...)
