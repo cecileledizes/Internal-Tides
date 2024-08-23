6715f85957e34ade0314ce4ff1bf677e2511bc32
jewel <jewelc1005@gmail.com>
2024-08-23 18:50:37 -0400

    Update topographies.jl

--- src/functions/topographies.jl
+++ src/functions/topographies.jl
@@ -8,6 +8,7 @@ using Oceananigans.Units
     # Gaussian topography
     @inline hill(x, y) = (sp.h₀)meters * exp((-x^2 - y^2)/ (2(((sp.width)meters)^2)))
     @inline bottom(x, y) = - (sp.H)meters + hill(x, y)
-    
-    topographies = (; gaussian = bottom(x, y))
+
+    # Return this
+    (; gaussian = bottom(x, y))
 end
