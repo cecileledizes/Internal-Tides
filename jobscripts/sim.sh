4493385306113f7d441f988673884136ef1225bb
jewel <jewelc1005@gmail.com>
2024-08-23 13:30:15 -0400

    Update sim.sh

--- jobscripts/sim.sh
+++ jobscripts/sim.sh
@@ -8,7 +8,7 @@ module load julia/1.10.4
 export JULIA_DEPOT_PATH=$SCRATCH/.julia-mist
 export JULIA_SCRATCH_TRACK_ACCESS=0
 
-cd ~/project
+cd ~/[FOLDER]
 
 # file to run, (; foldername, stop_time(days), H(m), Nx, Ny, Nz, h₀(m), width(m), ω₂(rad/s), Nᵢ²(s^-2), latitude/f)
 julia src/simulation/run_base_sim.jl ../scratch 2 3000 512 512 256 900 15558 "0.00014" "4e-6" 0
