1c67f964d8fe2cee93c04a7d1d266169d55d5413
jewel <jewelc1005@gmail.com>
2024-08-23 18:54:26 -0400

    Update relaxation_mask.jl

--- src/functions/relaxation_mask.jl
+++ src/functions/relaxation_mask.jl
@@ -4,9 +4,11 @@ using Oceananigans
 using Oceananigans.Units
 
 @inline function create_masks(sp::NamedTuple)
-    
+
+    # Gaussian mask
     bottom_mask = GaussianMask{:z}(center=(-(sp.H)meters + (sp.h₀)meters), width=(sp.h₀)meters)
-    
+
+    # Mask following the edge of the seamount
     @inline function bottom_mask_2(x, y, z)
         if z <= (bottom(x, y) + 10meters)
             1
@@ -14,8 +16,10 @@ using Oceananigans.Units
             0
         end
     end
-    
+
+    # Exponential mask
     @inline bottom_mask_3(x, y, z) = exp(-(z - (-(sp.H)meters + (sp.h₀)meters)) / (2 * (sp.h₀)meters))
-    
-    masks = (; mask_1 = bottom_mask, mask_2 = bottom_mask_2, mask_3 = bottom_mask_3)
+
+    # return this
+    (; mask_1 = bottom_mask, mask_2 = bottom_mask_2, mask_3 = bottom_mask_3)
 end
