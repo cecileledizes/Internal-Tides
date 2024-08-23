cdc0fbc14b64d7e4fc42863977f4e8ba91150d83
jewel <jewelc1005@gmail.com>
2024-08-23 19:13:59 -0400

    Update topographies.jl

--- src/functions/topographies.jl
+++ src/functions/topographies.jl
@@ -9,6 +9,10 @@ using Oceananigans.Units
     @inline hill(x, y) = (sp.h₀)meters * exp((-x^2 - y^2)/ (2(((sp.width)meters)^2)))
     @inline gaussian_bottom(x, y) = - (sp.H)meters + hill(x, y)
 
+    # Witch of Agnesi ridge
+    @inline ridge(x, y) = (sp.h₀)meters * (1 + (x^2/sp.width^2))^(-1)
+    @inline ridge_bottom(x, y) = - (sp.H)meters + ridge(x, y)
+
     # Return this
-    (; gaussian = gaussian_bottom)
+    (; gaussian = gaussian_bottom, ridge = ridge_bottom)
 end
