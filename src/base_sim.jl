9e8ba5b7f604e6145b32e52a3fc0f7f3f4ee8f8f
jewel <jewelc1005@gmail.com>
2024-08-23 13:48:36 -0400

    Update base_sim.jl

--- src/base_sim.jl
+++ src/base_sim.jl
@@ -9,7 +9,7 @@ include("functions/closures.jl")
 include("functions/grid_spacings.jl")
 include("functions/forcings.jl")
 
-@inline function it_create_simulation(stop_time::Number, foldername, simulation_parameters::NamedTuple)
+@inline function it_create_simulation(stop_time::Number, foldername, simulation_parameters::NamedTuple) # (simulation stop time, output folder for results, simulation parameters) 
     
     sp = create_simulation_parameters(simulation_parameters)
     z_spacing = create_spacings(sp)
