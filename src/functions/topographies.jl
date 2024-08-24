d8b68d6ab09f07cd79821b6c6cf6002f9450b24c
jewel <jewelc1005@gmail.com>
2024-08-23 21:18:46 -0400

    Update topographies.jl

--- src/functions/topographies.jl
+++ src/functions/topographies.jl
@@ -1,18 +1,24 @@
-# Create topography 
+# Create topography: create_gaussian_topography, create_ridge_topography 
 
 using Oceananigans
 using Oceananigans.Units
 
-@inline function create_topography_functions(sp::NamedTuple)
+@inline function create_gaussian_topography(sp::NamedTuple)
+    "Gaussian hill" 
     
-    # Gaussian topography
     @inline hill(x, y) = (sp.h₀)meters * exp((-x^2 - y^2)/ (2(((sp.width)meters)^2)))
     @inline gaussian_bottom(x, y) = - (sp.H)meters + hill(x, y)
 
-    # Witch of Agnesi ridge
+    # return this
+    gaussian_bottom
+end
+
+@inline function create_ridge_topography(sp::NamedTuple)
+    "Witch of Agnesi ridge" 
+    
     @inline ridge(x, y) = (sp.h₀)meters * (1 + (x^2/sp.width^2))^(-1)
     @inline ridge_bottom(x, y) = - (sp.H)meters + ridge(x, y)
 
-    # Return this
-    (; gaussian = gaussian_bottom, ridge = ridge_bottom)
+    # return this
+    ridge_bottom
 end
