f9430d73db53e46d34a8391ae91dfd8e9dd5be58
jewel <jewelc1005@gmail.com>
2024-08-24 09:58:25 -0400

    Update nondim_sim.sh

--- jobscripts/nondim_sim.sh
+++ jobscripts/nondim_sim.sh
@@ -10,5 +10,5 @@ export JULIA_SCRATCH_TRACK_ACCESS=0
 
 cd ~/[FOLDER]
 
-# COMMENT: file to run, (; foldername, stop_time, δ=0.3, E=0.5, β=0.5, B=14, H=3000, Nx=512, Ny=512, Nz=256, ω₂=0.00014)
-julia src/run_nondim_sim.jl ../[FOLDERNAME] 4 "0.3" "0.5" "0.5" 14 3000 512 512 256 "0.00014"
+# COMMENT: file to run, (; foldername, stop_time, δ=0.3, E=0.5, β=0.5, B=14, H=3000, Nx=512, Ny=512, Nz=256, ω₂=0.00014, L=15558)
+julia src/run_nondim_sim.jl ../[FOLDERNAME] 4 "0.3" "0.5" "0.5" 14 3000 512 512 256 "0.00014" 15558
