a14c93ed4fc70dfc5d9f33fdcfa66445683c2073
jewel <jewelc1005@gmail.com>
2024-08-23 21:09:02 -0400

    Update relaxation_mask.jl

--- src/functions/relaxation_mask.jl
+++ src/functions/relaxation_mask.jl
@@ -1,26 +1,27 @@
-# used in "forcings", not "base_sim" 
+# used in "forcings", not "base_sim": create_gaussian_mask, create_mask_2, create_exponential_mask
 
 using Oceananigans
 using Oceananigans.Units
 
-@inline function create_masks(sp::NamedTuple)
-    "masks for relaxation function" 
-    
-    # Gaussian mask
+@inline function create_gaussian_mask(sp::NamedTuple)
+    "Gaussian mask for relaxation function" 
+    # return this
     bottom_mask = GaussianMask{:z}(center=(-(sp.H)meters + (sp.h₀)meters), width=(sp.h₀)meters)
+end
 
-    # Mask following the edge of the seamount
-    @inline function bottom_mask_2(x, y, z)
+@inline function create_mask_2(bottom::Function)
+    "Mask following the edge of the seamount"
+    @inline function bottom_mask_2(x, y, z) 
         if z <= (bottom(x, y) + 10meters)
             1
         else 
             0
         end
     end
+    bottom_mask_2
+end
 
-    # Exponential mask
-    @inline bottom_mask_3(x, y, z) = exp(-(z - (-(sp.H)meters + (sp.h₀)meters)) / (2 * (sp.h₀)meters))
-
-    # return this
-    (; mask_1 = bottom_mask, mask_2 = bottom_mask_2, mask_3 = bottom_mask_3)
+@inline function create_exponential_mask(sp::NamedTuple)
+    "Exponential mask" 
+    bottom_mask_3(x, y, z) = exp(-(z - (-(sp.H)meters + (sp.h₀)meters)) / (2 * (sp.h₀)meters))
 end
