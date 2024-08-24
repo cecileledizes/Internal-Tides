e2b78e22f6e4962f0be6d12d954c6825d8840235
jewel <jewelc1005@gmail.com>
2024-08-23 21:24:42 -0400

    Update parameters.jl

--- src/functions/parameters.jl
+++ src/functions/parameters.jl
@@ -1,7 +1,7 @@
 # Creates a NamedTuple of simulation parameters
 
-default_inputs = (; H=3000, Nx=512, Ny=512, Nz=256, h₀=900, width=15558, ω₂=0.00014, Nᵢ²=4e-6, latitude=0)
-# meters, no units, no units, no units, meters, meters, rad/sec, s⁻², f
+default_inputs = (; H=3000, Nx=512, Ny=512, Nz=256, h₀=900, width=15558, ω₂=0.00014, Nᵢ²=4e-6, f=0)
+# meters, no units, no units, no units, meters, meters, rad/sec, s⁻², rad/sec
 
 @inline function create_simulation_parameters(input_parameters=(; ))
     ip = (; default_inputs..., input_parameters...)
